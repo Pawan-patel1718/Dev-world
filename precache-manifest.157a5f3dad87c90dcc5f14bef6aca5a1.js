74db1000526a5f7d6ee8b68d730356a894a74bbf
Pawan Patel <pp895006@gmail.com>
2022-07-22 10:19:59 +0530

    Updates

---
+++ precache-manifest.157a5f3dad87c90dcc5f14bef6aca5a1.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "4d6d4eb31bca29a9833a",
+    "url": "/Dev-world/static/css/main.868c86af.chunk.css"
+  },
+  {
+    "revision": "4d6d4eb31bca29a9833a",
+    "url": "/Dev-world/static/js/main.d6c94298.chunk.js"
+  },
+  {
+    "revision": "f00ad87aaea371b56f7c",
+    "url": "/Dev-world/static/js/runtime~main.44f604a7.js"
+  },
+  {
+    "revision": "df304792b4aaeb731949",
+    "url": "/Dev-world/static/css/2.fac1c19e.chunk.css"
+  },
+  {
+    "revision": "df304792b4aaeb731949",
+    "url": "/Dev-world/static/js/2.1e03bc52.chunk.js"
+  },
+  {
+    "revision": "2ed70a1a2df1f93a1f29",
+    "url": "/Dev-world/static/js/3.9209433f.chunk.js"
+  },
+  {
+    "revision": "7fc21faa4948641b89a5a45e1339e290",
+    "url": "/Dev-world/static/media/Isometric laptop with chart and financial growth graph.7fc21faa.jpg"
+  },
+  {
+    "revision": "ec7442c86f31ef9265146e390511defb",
+    "url": "/Dev-world/static/media/Business man 01.ec7442c8.jpg"
+  },
+  {
+    "revision": "42a8c5d15874cb3e8dee12cf9a53b21b",
+    "url": "/Dev-world/static/media/web dev.42a8c5d1.jpg"
+  },
+  {
+    "revision": "97cfce3da4513c0a332c81ee683236aa",
+    "url": "/Dev-world/static/media/android .97cfce3d.jpg"
+  },
+  {
+    "revision": "ab3614e8c6794d83f1b80cbbc6fd537a",
+    "url": "/Dev-world/static/media/web devlopment.ab3614e8.webp"
+  },
+  {
+    "revision": "6c742a8bde531f8df536f8439a97d2a9",
+    "url": "/Dev-world/index.html"
+  }
+];
